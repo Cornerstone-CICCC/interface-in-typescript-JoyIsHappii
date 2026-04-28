dd3021a994a6bb3ab1ff518f530ff89674693bcf
JoyIsHappii <chayaninkosol@gmail.com>
2026-04-28 12:32:00 -0700

    ex05: done

---
+++ exercises/ex05.ts
@@ -0,0 +1,26 @@
+// Exercise: Creating Intersection Types
+
+// Define types Admin and Employee with appropriate properties of your choice.
+// Create an intersection type AdminEmployee that combines properties from both Admin and Employee.
+// Instantiate an object using the AdminEmployee type and display the properties.
+
+type Admin = {
+  adminId: number;
+  adminName: string;
+};
+
+type Employee = {
+  employeeId: number;
+  employeeName: string;
+};
+
+type AdminEmployee = Admin & Employee;
+
+const adminEmployee1: AdminEmployee = {
+  adminId: 1,
+  adminName: "Alice",
+  employeeId: 101,
+  employeeName: "Bob",
+};
+
+console.log(adminEmployee1);
