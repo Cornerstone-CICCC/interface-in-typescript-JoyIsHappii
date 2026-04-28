b260ebb36e280e73abba6587faca57c71568064e
JoyIsHappii <chayaninkosol@gmail.com>
2026-04-28 13:07:46 -0700

    ex11: done

---
+++ exercises/ex11.ts
@@ -0,0 +1,9 @@
+// Exercise: Nullish Coalescing
+// Create a variable userInput with a null value.
+// Use nullish coalescing to set a default value Please enter something... if userInput is null or undefined.
+// Display the resulting value.
+
+const userInput: string | null = null;
+const result = userInput ?? "Please enter something...";
+
+console.log(result);
